8307a90c4ed4def93bc5ed9bc4b44baea2905392
Dhrumil Bhatt <techpanditz@gmail.com>
2025-12-01 12:58:13 +0000

    Added prodution url

--- frontend/src/pages/ApiDocs.jsx
+++ frontend/src/pages/ApiDocs.jsx
@@ -2,7 +2,7 @@ export default function ApiDocs() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="http://localhost:4000/api-docs"
+        src="https://hospital-saas-hackathon.onrender.com/api-docs/"
         title="Hospital SaaS API Docs"
         style={{
           width: "100%",
